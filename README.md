88667b2d6f675eb6071130dfe66f27676bcf681b
kbatchelor <kbatchelor@insidesales.com>
2016-10-17 11:29:42 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# shModalDialog
+# SuperheroUI [![Build Status](https://travis-ci.org/SuperheroUI/shModalDialog.svg?branch=master)](https://travis-ci.org/SuperheroUI/shModalDialog) [![Coverage Status](https://coveralls.io/repos/github/SuperheroUI/shModalDialog/badge.svg)](https://coveralls.io/github/SuperheroUI/shModalDialog) [![npm version](https://badge.fury.io/js/sh-modal-dialog.svg)](https://badge.fury.io/js/sh-modal-dialog)
+InsideSales.com React UI
+
+## ShModalDialog
+
+### Website (For examples, limits, and usage)
+https://superheroui.github.io/
+
+### Installation
+Use npm to install the module
+```sh
+npm install sh-modal-dialog --save
+```
+
+Now you can import it into your project
+```js
+import ShModalDialog from 'sh-modal-dialog';
+```
+
---
+++ src/sh-modal-dialog.js
@@ -0,0 +1,117 @@
+import React, {Component} from 'react';
+import ShCore from 'sh-core';
+import './sh-modal-dialog.scss';
+
+class ShModalDialog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            child: null,
+            classList: {
+                displayModal: false,
+                fadeIn: false,
+                firstRun: true
+            }
+        };
+
+        this.showModal = this.showModal.bind(this);
+        this.hideModal = this.hideModal.bind(this);
+        this.setData = this.setData.bind(this);
+    }
+
+    componentWillReceiveProps(props) {
+        if ((this.state.classList.displayModal === false) && (props.shShowModal)) {
+            this.props.shResetModal();
+            this.showModal();
+        }
+
+        if ((this.state.classList.displayModal === true) && (props.shHideModal)) {
+            this.hideModal()();
+        }
+    }
+
+    showModal() {
+        this.setState({
+            classList: {
+                displayModal: true
+            }
+        }, ()=> {
+            setTimeout(()=> {
+                this.setState({
+                    classList: {
+                        displayModal: true,
+                        fadeIn: true
+                    }
+                })
+            }, 100)
+        })
+    }
+
+    componentDidMount() {
+        this.setState({
+            child: React.cloneElement(this.props.children, {returnData: {}})
+        })
+    }
+
+    setData(returnData) {
+        if (returnData) {
+            this.props.shReturnData(this.state.child.props.returnData)
+        }
+    }
+
+    hideModal(returnData) {
+        return ()=> {
+            this.setState({
+                classList: {
+                    fadeIn: false,
+                    displayModal: true
+                }
+            }, ()=> {
+                setTimeout(()=> {
+                    this.setState({
+                        classList: {
+                            displayModal: false,
+                            fadeIn: false
+                        }
+                    }, this.setData(returnData))
+                }, 1000)
+            })
+        }
+    }
+
+    getSaveButton() {
+        if (this.props.shSaveButton) {
+            return this.props.shSaveButton
+        } else {
+            return <button className="sh-btn sh-btn-primary" onClick={this.hideModal(true)}>Save</button>
+        }
+    }
+
+    render() {
+        return (
+            <div className={'sh-modal-dialog ' + ShCore.getClassNames(this.state.classList)}>
+                <div className="sh-modal-content ">
+                    <i className="sh-icon icon-x sh-close" onClick={this.hideModal()}/>
+                    <div className="sh-modal-title">{this.props.shModalTitle}</div>
+                    <div className="sh-dynamic-content">
+                        {this.state.child}
+                    </div>
+                    <div className="sh-button-divider"/>
+                    <div className="sh-button-group">
+                        <button className="sh-btn sh-btn-default sh-cancel" onClick={this.hideModal()}>Cancel</button>
+                        {this.getSaveButton()}
+                    </div>
+                </div>
+            </div>
+        )
+    }
+}
+
+ShModalDialog.propTypes = {
+    shSaveButton: React.PropTypes.any,
+    shShowModal: React.PropTypes.bool,
+    shHideModal: React.PropTypes.bool,
+    shResetModal: React.PropTypes.func.isRequired
+};
+
+export default ShModalDialog;
